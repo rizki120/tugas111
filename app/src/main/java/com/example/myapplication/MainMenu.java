b874092d2b962602fa45bf301f9031091f650ec6
unknown <emailgithubmu@gmail.com>
2020-09-05 23:19:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainMenu.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainMenu extends AppCompatActivity {
+    CardView menu1;
+    CardView menu2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainmenu);
+        menu1 = (CardView)findViewById(R.id.menu1);
+        menu1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), listdata.class));
+            }
+        });
+        menu2 = (CardView)findViewById(R.id.menu2);
+    }
+}
